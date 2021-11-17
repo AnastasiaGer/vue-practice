e6c9986f1e0fd15ddb4c1bb3af4b00ea2bbc0a9e
AnastasiaGer <aagermanova@gmail.com>
2021-11-16 16:09:39 -0800

    2 step

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: 'Counter:'
+      placeholderString: "Add task text",
+      title: 'Task list',
+      inputValue: '',
+      notes: ['1 Note', '2 Note'],
     }
   },
+  methods: {
+    addNewNote() {
+      if(this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1)
+    },
+    toUpperCase(item) {
+      return item.toUpperCase()
+    },
+  },
+  computed: {
+    doubleCountComputed() {
+      return this.notes.length * 2
+    }
+  },
+  watch: {
+    inputValue(value) {
+
+    }
+  }
 };
 
 Vue.createApp(App).mount('#app')
